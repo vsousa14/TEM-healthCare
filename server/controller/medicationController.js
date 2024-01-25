3825722bf4cbc17a2a5571fcc581a8fcd0583894
Guilherme Sousa <jgmsousa12@gmail.com>
2024-01-25 00:24:19 +0000

    Co-authored-by: Leo <leofgitz@users.noreply.github.com>

--- server/controller/medicationController.js
+++ server/controller/medicationController.js
@@ -3,9 +3,63 @@ import Medication from "../model/medicationModel";
 const err500 = "Erro Interno de Servidor";
 
 const MedicationController = {
-  getPrescription: async (req, res) => {},
-  createPrescription: async (req, res) => {},
-  deletePrescription: async (req, res) => {},
-  requestPrescription: async (req, res) => {},
-  sendPrescription: async (req, res) => {},
+  getAllPrescriptions: async (req, res) => {
+    try {
+      const u_id = req.params.id;
+
+      const prescriptions = await Medication.findAll({
+        where: { u_id: u_id },
+      });
+
+      if (!prescriptions){
+        return res.status(404).json({ error: "Utilizador não encontrado." });
+      }
+
+      res.status(200).json(prescriptions);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: err500 });
+    }
+  },
+
+  createPrescription: async (req, res) => {
+    try {
+      const newPrescription = await Medication.create(req.body);
+      res.status(201).json(newPrescription);
+
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: err500 });
+    }
+  },
+
+  deletePrescription: async (req, res) => {
+    try {
+      const pills_id = req.params.prescriptionId;
+      const deletedRows = await Medication.destroy({
+        where: { pills_id: pills_id },
+      });
+
+      if (deletedRows === 0) {
+        return res.status(404).json({ error: "Prescrição não encontrada." });
+      }
+
+      res.status(204).send();
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: err500 });
+    }
+  },
+
+  
+  requestPrescription: async (req, res) => {
+    // Implementar lógica de solicitação de prescrição aqui
+  },
+  
+  // Later
+  sendPrescription: async (req, res) => {
+    // Implementar lógica de envio de prescrição aqui
+  },
 };
+
+module.exports = MedicationController;
